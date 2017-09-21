16d0700c775695c2315d0f5f277de0e813ecd29b
Sascha Hoppe <kn0rki@users.noreply.github.com>
2017-09-21 16:16:40 +0200

    Comment failure.

--- OnBattery.ps1
+++ OnBattery.ps1
@@ -2,8 +2,8 @@ import-module 'C:\Program Files\DataCore\SANsymphony\DataCore.Executive.Cmdlets.
 $logfile = "$env:temp\USV.log"
 Add-Content -Path $logfile -Value (get-date) -NoNewline
 Add-Content -Path $logfile -Value " - DCS Battery"
-#$connection = Connect-DcsServer localhost
+$connection = Connect-DcsServer localhost
 if($connection){
     Disable-DcsServerWriteCache -Server localhost
 }
-#Disconnect-DcsServer -Connection $connection
+Disconnect-DcsServer -Connection $connection
