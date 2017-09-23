ca9340b4a021122882b18d95b1ca08c657a65aa0
Sascha Hoppe <kn0rki@users.noreply.github.com>
2017-09-23 13:44:02 +0200

    Update OnBattery.ps1
    forgot something :\

--- OnBattery.ps1
+++ OnBattery.ps1
@@ -1,7 +1,8 @@
 import-module 'C:\Program Files\DataCore\SANsymphony\DataCore.Executive.Cmdlets.dll' -DisableNameChecking -ErrorAction Stop
 $logfile = "$env:temp\USV.log"
 Add-Content -Path $logfile -Value (get-date)
-Add-Content -Path $logfile -Value " - DCS Battery"
+Add-Content -Path $logfile -Value "DCS Battery"
+Add-Content -Path $logfile -Value ""
 $connection = Connect-DcsServer localhost
 if($connection){
     Disable-DcsServerWriteCache -Server (hostname)
