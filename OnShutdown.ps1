4a29b9d4246afdd600e0362427964be1655db5f5
Sascha Hoppe <kn0rki@users.noreply.github.com>
2017-09-23 13:44:30 +0200

    Update OnShutdown.ps1
    Forgot something :\

--- OnShutdown.ps1
+++ OnShutdown.ps1
@@ -1,7 +1,8 @@
 import-module 'C:\Program Files\DataCore\SANsymphony\DataCore.Executive.Cmdlets.dll' -DisableNameChecking -ErrorAction Stop
 $logfile = "$env:temp\USV.log"
 Add-Content -Path $logfile -Value (get-date) -NoNewline
-Add-Content -Path $logfile -Value " - DCS Shutdown"
+Add-Content -Path $logfile -Value "DCS Shutdown"
+Add-Content -Path $logfile -Value ""
 $connection = Connect-DcsServer localhost
 if($connection){
     Stop-DcsServer -Server (hostname)
